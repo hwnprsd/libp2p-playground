c762465bc5754537f3904f5ca565487c46aa0734
Ashwin Prasad <ashwin@onpar.in>
2023-10-07 18:58:57 +0530

    added testcases

--- rules/rules_test.go
+++ rules/rules_test.go
@@ -11,32 +11,40 @@ var (
 	walletAddr = "0xeb92E75eCb3fcB7e6Ef99008a126A3EB51Fbe512"
 	sender1    = "0xdFFf55db7904095FfE5F5e850A8EaF072661Fa50"
 	sender2    = "0x64D806699127ee59F0AD1578f8098BcD6e095305"
+	sender3    = "0x1429430ca11986674aAB130525E2aec42AD5DC97"
+
+	senderGroup1 = &proto.SenderGroup{
+		Addresses: []string{sender1, sender2},
+		Name:      "TEST_SENDER_1",
+	}
+
+	senderGroup2 = &proto.SenderGroup{
+		Addresses: []string{sender2, sender3},
+		Name:      "TEST_SENDER_2",
+	}
 
 	tx = &proto.SolaceTx{
 		Sender:     &proto.Sender{Addr: sender1, Nonce: 0},
 		ToAddr:     "TO_ADDR_1",
 		TokenAddr:  "TOKEN_ADDR_1",
-		Value:      1000,
+		Value:      101,
 		WalletAddr: walletAddr,
 	}
 
 	rules = []*proto.AccessControlRule{
 		{
-			WalletAddr: walletAddr,
-			SenderGroup: &proto.SenderGroup{
-				Addresses: []string{sender1, sender2},
-				Name:      "TEST_SENDER_1",
-			},
+			WalletAddr:       walletAddr,
 			TokenAddress:     "TOKEN_ADDR_1",
 			RecipientAddress: "TO_ADDR_1",
+			SenderGroup:      senderGroup1,
 		}, {
-			WalletAddr: walletAddr,
-			SenderGroup: &proto.SenderGroup{
-				Addresses: []string{sender1, sender2},
-				Name:      "TEST_SENDER_2",
+			WalletAddr:   walletAddr,
+			SenderGroup:  senderGroup1,
+			TokenAddress: "TOKEN_ADDR_2",
+			ValueRangeClause: &proto.ValueRangeClause{
+				MinVal: 100,
+				MaxVal: 1000,
 			},
-			TokenAddress:     "TOKEN_ADDR_2",
-			RecipientAddress: "TO_ADDR_1",
 		},
 	}
 )
@@ -48,6 +56,8 @@ func Test_GetRules(t *testing.T) {
 		t.Error(err)
 	}
 	t.Log("//", sender1, "//", ethSenderAddr.String())
-	r1, r2, r3 := GetRulesForSender(tx, ethSenderAddr, rules)
-	t.Log(len(r1), len(r2), len(r3))
+	err = GetRulesForSender(tx, ethSenderAddr, rules)
+	if err != nil {
+		t.Error(err)
+	}
 }
